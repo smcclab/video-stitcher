ed80e5a8adf40bc3d9c5d01b03ddcfc06cdb3889
Charles Martin <cpm@charlesmartin.com.au>
2025-06-17 22:56:57 +1000

    fixing..

--- video_stitcher/__main__.py
+++ video_stitcher/__main__.py
@@ -18,8 +18,12 @@ data_path = Path("data")
 
 def load_data(datafile="data.csv"):
     with open(data_path / datafile, newline='') as csvfile:
-      return list(csv.DictReader(csvfile))
-
+      data_list = list(csv.DictReader(csvfile))
+    # format,presence
+    # poster,remote,
+    data_list = [row for row in data_list if row['format'] == 'poster' and row['presence'] == 'remote']
+    # data_list = [row for row in data_list if row['presence'] == 'remote']
+    return data_list
 
 def create_directories():
     """
@@ -45,18 +49,23 @@ def video_path(video_id, verbose=False):
             return None
 
 
-def build_video_lists(verbose=False):
+id_col = "id"
+title_col = "title"
+authors_col = "authors-names"
+session_col = "session_code"
+
+
+def build_video_lists():
   data = load_data()
-  sessions = {row['session'] for row in data} # set of sessions
+  sessions = {row[session_col] for row in data} # set of sessions
+  click.secho(f"Sessions {sessions}", fg="blue")
   video_dict = {}
   for s in sessions:
-    if verbose: 
-      click.secho(f"Finding video for ID {video_id}.", fg="yellow")
-    video_list = [(video_path(entry['id']), entry['title'], entry['authors']) for entry in data if entry['session'] == s]
-    video_list = [(path, title) for (path, title) in video_list if path is not None]
+    click.secho(f"Finding video for {s}.", fg="yellow")
+    video_list = [(video_path(entry[id_col]), entry[title_col], entry[authors_col]) for entry in data if entry[session_col] == s]
+    video_list = [(path, title, authors) for (path, title, authors) in video_list if path is not None]
     video_dict[s] = video_list   
-    if verbose: 
-      click.secho(f"{s}: {video_list}", fg="yellow")     
+    click.secho(f"{s}: {video_list}", fg="yellow")     
     return video_dict
 
 
@@ -67,7 +76,7 @@ def render(verbose=True):
     """
     create_directories()
     click.secho(f"Build the video lists.", fg='green')
-    video_dict = build_video_lists(verbose=verbose)
+    video_dict = build_video_lists()
     click.secho("Going to collate the videos.", fg="blue")
     # video_dict has the structure:
     # {session_name: [(path, title, authors), (path, title, authors),...], ...}
